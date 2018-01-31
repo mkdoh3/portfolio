6fc77d4954caf1f858ba268e51f702cfc86bd00f
mkdoh3 <mkdoherty3@GMAIL.COM>
2018-01-31 11:36:01 -0600

    pointer double render bug fixed

--- src/components/type/type.js
+++ src/components/type/type.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import Typist from 'react-typist';
 
-export default class Type extends Component {
+const Type = () => {
+    return (
+        <Typist>
+            <span>Hi, I'm Michael.</span><br/>
+            <span>I'm a learner.</span>
+            <Typist.Backspace count={8} delay={20} />
+            <span>thinker.</span>
+            <Typist.Backspace count={8} delay={40} />
+            <span>creator.</span>
+            <Typist.Backspace count={8} delay={200} />
+            <span style={{fontWeight: 'bold'}}>web developer.</span>
+        </Typist>
+    )
+}
 
-    render() {
-        return (
-            <Typist>
-                <span>Hi, I'm Michael.</span><br/>
-                <span>I'm a learner.</span>
-                <Typist.Backspace count={8} delay={20} />
-                <span>thinker.</span>
-                <Typist.Backspace count={8} delay={40} />
-                <span>creator.</span>
-                <Typist.Backspace count={8} delay={100} />
-                <span style={{fontWeight: 'bold'}}>web developer.</span>
-            </Typist>
-        )
-    }
-} 
+export default Type;
